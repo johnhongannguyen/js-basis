ca3ad1801bb0cd98d5ab7495ef0b75830d609f7c
John Nguyen <50645235+johnhongannguyen@users.noreply.github.com>
2021-01-29 02:53:04 -0800

    Add files via upload
    basics of object

---
+++ basics.js
@@ -0,0 +1,25 @@
+// let radius = 1;
+// let x = 1;
+// let y = 1;
+
+const circle = {
+    radius: 1 ,
+    location : {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function () {
+        console.log('draw');
+    },
+    move: function() {
+        console.log('move')
+    }
+};
+// we don't need these function for a circle object
+// function draw () {
+
+// }
+// function move () {
+
+// }
