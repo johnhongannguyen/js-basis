be55a1808a9c2e5497d77883a931b39ed945629a
John Nguyen <50645235+johnhongannguyen@users.noreply.github.com>
2021-01-28 23:43:37 -0800

    Add files via upload
    polish code

--- primeNumbers.js
+++ primeNumbers.js
@@ -1,14 +1,18 @@
 showPrimes(500);
 
 function showPrimes(limit){
-    for(let number = 2; number <= limit; number++){
-        let isPrime = true;
-        for(factor = 2; factor < number; factor++){
-            if(number % factor === 0){
-                isPrime = false;
+    for(let number = 2; number <= limit; number++)
+        if(isPrime(number)) console.log(number);
 
-            }
-        }
-        if(isPrime) console.log(number);
+}
+
+function isPrime(number){
+    
+    
+    for(factor = 2; factor < number; factor++){
+        if(number % factor === 0)
+        return false
+           
     }
+    return true;
 }
