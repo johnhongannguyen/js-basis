8cd2051257f29f59a2c8e3fb015c5cdf45d25525
John Nguyen <50645235+johnhongannguyen@users.noreply.github.com>
2021-01-31 00:14:17 -0800

    Add files via upload
    enumerate properties

---
+++ enumerateProperties.js
@@ -0,0 +1,17 @@
+const circle = {
+    radius: 1,
+    draw () {
+        console.log('drawing');
+    }
+};
+
+for(let key in circle)
+console.log(key, circle[key]);
+
+for(let key of Object.keys(circle))
+console.log(key);
+
+for(let entry of Object.entries(circle))
+console.log(entry);
+
+if('radius' in circle) console.log('yes');
