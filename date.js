9505ded671fbdaa22d8c002c0121d22834c6002c
John Nguyen <50645235+johnhongannguyen@users.noreply.github.com>
2021-02-02 08:28:29 -0800

    Add files via upload

---
+++ date.js
@@ -0,0 +1,10 @@
+// Date is constructor function
+const now = new Date();
+const date1 = new Date('May 11 2018 09:00');
+const date2 = new Date(2018, 4, 11, 9, 0);
+
+now.setFullYear(2017);
+
+// now.toDateString()
+// now.toTimeString()
+// now.toISOString()
