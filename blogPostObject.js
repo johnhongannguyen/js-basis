4512b16d5912aa88b569467a9d65a090808f3187
John Nguyen <50645235+johnhongannguyen@users.noreply.github.com>
2021-02-03 11:23:53 -0800

    Add files via upload
    get familiar with the literal syntax

---
+++ blogPostObject.js
@@ -0,0 +1,26 @@
+// title
+// body
+// author 
+// views 
+// comments
+// (author, body)
+// isLive
+
+const blog = {
+    title: 'This is my first blog',
+    body: 'There is a lot of things need to tell you in my post',
+    author: 'John Nguyen',
+    views: 5, 
+    comments: [
+        {
+            author: 'Dean',
+            body: 'great'
+        },
+        {
+            author: 'Don',
+            body: 'keep working'
+        }
+    ],
+    isLive: true,
+}
+console.log(blog);
