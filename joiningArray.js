f5484e8aee3d78d354d18da94f5473b3890c2962
John Nguyen <50645235+johnhongannguyen@users.noreply.github.com>
2021-02-06 20:02:04 -0800

    Add files via upload

---
+++ joiningArray.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3];
+
+const joined = numbers.join(',');
+console.log(joined);
+
+const message = 'This is my first message';
+// using split() to create an array
+const parts = message.split(' ');
+console.log(parts);
+// joining to a string
+const combined = parts.join('-');
+console.log(combined);
