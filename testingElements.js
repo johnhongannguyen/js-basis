e36e053509551cc89055292a2f85877f45a82956
John Nguyen <50645235+johnhongannguyen@users.noreply.github.com>
2021-02-08 00:34:33 -0800

    Add files via upload
    map an object, and chaining method

--- testingElements.js
+++ testingElements.js
@@ -16,15 +16,20 @@ const numbers = [-1,-2,1, 2,3];
 
 // console.log(atLeastOnePositive);
 
-const filtered = numbers.filter(value => value >= 0);
+// const filtered = numbers.filter(value => value >= 0);
 
 // mapping an array 
 // mapping to a string
-const item = filtered.map(n => '<li>' + n + '</li>');
-const html = '<ul>' + item.join('') + '</ul>';
-console.log(filtered);
+// const item = filtered.map(n => '<li>' + n + '</li>');
+const item = numbers
+    .filter(n => n >= 0)
+    .map(n => ({value: n}))
+    .filter(obj => obj.value > 1)
+    .map(obj => obj.value);
+// const html = '<ul>' + item.join('') + '</ul>';
+// console.log(filtered);
 console.log(item);
-console.log(html);
+// console.log(html);
 
 
 
