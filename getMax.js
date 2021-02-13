95556f0c0aff12fe9fcd1b54f0a56ac24f4931a5
John Nguyen <50645235+johnhongannguyen@users.noreply.github.com>
2021-02-12 23:12:03 -0800

    Add files via upload

--- getMax.js
+++ getMax.js
@@ -1,11 +1,26 @@
 const numbers = [1,2,3,4];
 
-const max = getMax(numbers);
+const array1 = [22,33,55,66];
+const array2 = [21,4,3,5,100];
+
+const max = getMax(array2);
 
 console.log(max);
 
 function getMax(array){
+    // if(array.length === 0) return undefined;
+    // for(let e of array)
+    //     console.log(Math.max(...array));
     
-    for(let e of array)
-        console.log(Math.max(...array),e);
+    // solution 1
+    // if(array.length === 0) return undefined;
+
+    // let max = array[0];
+    // for(let i = 1; i < array.length ; i++)
+    //     if(array[i] > max)
+    //     max = array[i]
+    //     return max;
+
+    //solution 2
+    return array.reduce((accumulator,current) => (accumulator,current) ? accumulator : current);
 }
