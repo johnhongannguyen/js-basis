74ebb989a6e7fb9c230c0b17e67eeacbc81123e5
John Nguyen <50645235+johnhongannguyen@users.noreply.github.com>
2021-02-15 23:49:52 -0800

    Add files via upload
    Hoisting

--- declarationAndExpression.js
+++ declarationAndExpression.js
@@ -1,9 +1,11 @@
 // function declaration
+walk(); // can define it before it is defined 
 function walk() {
     console.log('walk')
 }
 
 // function expression 
+// but it doesn't work for function expression - uncaught reference error 
 let run = function() {
     console.log('run');
 };
@@ -11,3 +13,12 @@ let run = function() {
 let move = run;
 run ();
 move ();
+
+// what is hoisting in Javascript 
+// Hoisting is the process of moving function declarations to the top of the file
+// and this is automatically by the Javascript engine that is executing this code
+// so that's the reason we can call functions that are defined using the function declaration syntax before definition
+
+
+
+
